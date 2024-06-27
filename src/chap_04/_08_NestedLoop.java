4586ba3f958bc56bfde6ae0ff912e7ee7ffad947
may-30 <maius.triginta@gmail.com>
2024-06-27 20:51:05 +0900

    24.06.27 | chap_04 - 08 Nested Loop (2)

--- src/chap_04/_08_NestedLoop.java
+++ src/chap_04/_08_NestedLoop.java
@@ -21,5 +21,18 @@ public class _08_NestedLoop {
             }
             System.out.println();
         }
+
+        System.out.println("----------");
+
+        // 별 (*) 오른쪽 삼각형 만들기
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
