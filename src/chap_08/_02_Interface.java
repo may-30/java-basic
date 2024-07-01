540be90c367d750765de2bd91371029693427340
may-30 <maius.triginta@gmail.com>
2024-07-01 21:41:29 +0900

    24.07.01 | chap_08 - 02 Interface (2)

--- src/chap_08/_02_Interface.java
+++ src/chap_08/_02_Interface.java
@@ -1,5 +1,10 @@
 package chap_08;
 
+import chap_08.camera.FactoryCam;
+import chap_08.detector.AdvancedFireDetector;
+import chap_08.detector.Detectable;
+import chap_08.detector.FireDetector;
+
 /*
 import chap_08.reporter.NormalReporter;
 import chap_08.reporter.VideoReporter;
@@ -23,5 +28,22 @@ public class _02_Interface {
 
         Reportable videoReporter = new VideoReporter();
         videoReporter.report();
+
+        System.out.println("----------");
+
+        Detectable fireDetector = new FireDetector();
+        fireDetector.detect();
+
+        Detectable advancedFireDetector = new AdvancedFireDetector();
+        advancedFireDetector.detect();
+
+        System.out.println("----------");
+
+        FactoryCam factoryCam = new FactoryCam();
+        factoryCam.setDetector(advancedFireDetector);
+        factoryCam.setReporter(videoReporter);
+
+        factoryCam.detect();
+        factoryCam.report();
     }
 }
