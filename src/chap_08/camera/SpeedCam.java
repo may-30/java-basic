bae7de3dd3666131dd8d2a1c73101951a8b4817f
may-30 <maius.triginta@gmail.com>
2024-07-01 22:12:04 +0900

    24.07.01 | chap_08 - Quiz 08

--- src/chap_08/camera/SpeedCam.java
+++ src/chap_08/camera/SpeedCam.java
@@ -1,16 +1,33 @@
 package chap_08.camera;
 
-public class SpeedCam extends Camera {
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
+public class SpeedCam extends Camera implements Detectable, Reportable {
+    private Detectable detector;
+
+    private Reportable reporter;
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
+
     @Override
     public void showMainFeature() {
         System.out.println("속도 측정, 번호 인식");
     }
 
+    @Override
     public void detect() {
-        System.out.println("사고를 감지합니다.");
+        detector.detect();
     }
 
+    @Override
     public void report() {
-        System.out.println("사고 신고를 진행합니다.");
+        reporter.report();
     }
 }
